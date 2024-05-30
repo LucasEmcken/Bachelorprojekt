c0f12732cce672d2b8957f237e1f27f20df3f7ab
Lucas Sylvester <lucassylvester02@gmail.com>
2024-05-30 13:27:56 +0200

    Create artificial_experiment.py

---
+++ artificial_experiment.py
@@ -0,0 +1,39 @@
+#from shiftNMF_frozen import ShiftNMF
+from shiftNMFDiscTau import ShiftNMF
+from nnls_l1 import nnls
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+from helpers.generators import *
+from helpers.data import X_ART, X_ART_NOISY, TAU_ART, W_ART, H_ART
+from Hardmodel_single_peaks import Single_Model
+from Hardmodel import Hard_Model
+import scipy
+from scipy.signal import find_peaks
+import itertools
+from helpers.fit_functions import *
+
+
+nmf = ShiftNMF(X_ART_NOISY, 3, lr=0.1, alpha=1e-6, patience=30, min_imp=0.001)
+W_est, H_est, tau_est = nmf.fit(verbose=1, max_iter=2000, tau_iter=0)
+
+plt.plot(H_est.T)
+plt.savefig("shiftcomponents")
+
+for i in range(len(H_est)):
+    means, sigmas, n = single_fit(H_est[i])
+    #diff_matrix = calc_difference_matrix(sigmas)
+    diff_matrix = calc_difference_matrix(np.ones((len(sigmas))))
+    hypothesis = peak_hypothesis(diff_matrix, cutoff=10/100)
+    hardmodel = Hard_Model(H_est[i], hypothesis, means, sigmas, n, lr=10, alpha = 1e-3, factor=1, patience=1, min_imp=0.01)
+    W, C = hardmodel.fit(verbose=True, alpha=0.2)
+    print("W:")
+    print(W)
+    ## TODO change this to be the true underlying components from H_ART not the shift found components, they need to be scaled accordingly.
+    plt.plot(H_est[i]/np.std(H_est[i]), linewidth=5)
+
+    for j, vec in enumerate(C):
+        plt.plot(vec*W[:,j])
+    plt.title("Component "+str(i)+" hardmodelled")
+    plt.savefig("fig"+str(i))
