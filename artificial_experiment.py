088f00b84643fd27ef965057a35b0639e4d42f2e
Lucas Sylvester <lucassylvester02@gmail.com>
2024-06-03 01:19:00 +0200

    Update artificial_experiment.py

--- artificial_experiment.py
+++ artificial_experiment.py
@@ -14,11 +14,12 @@ from scipy.signal import find_peaks
 import itertools
 from helpers.fit_functions import *
 import torch
+from helpers.plot import *
 
 #limit X_ART_NOISY to 15 components
 # X_ART_NOISY = X_ART_NOISY[:5]
 
-torch.manual_seed(42)
+torch.manual_seed(40)
 
 nmf = ShiftNMF(X_ART_NOISY, 3, lr=0.1, alpha=1e-6, patience=30, min_imp=0.001)
 W_est, H_est, tau_est = nmf.fit(verbose=1, max_iter=2000, tau_iter=0)
@@ -26,11 +27,40 @@ W_est, H_est, tau_est = nmf.fit(verbose=1, max_iter=2000, tau_iter=0)
 # W_est = W_ART
 # H_est = H_ART
 # tau_est = TAU_ART
+# Create a gridspec object with 2 rows and 3 columns
+gs = gridspec.GridSpec(1, 3, width_ratios=[3, 1, 1])
+
+fig = plt.figure(figsize=(10, 6))
+
+# Plot H and H_est
+ax1 = plt.subplot(gs[0])
+# plotSpaced(ax1, np.arange(H_est.shape[1]), H_est.T)
+ax1.set_title('H_est')
+
+ax2 = plt.subplot(gs[1])
+ax2.set_title('W_est')
+hinton(W_est.T, ax=ax2)
+
+ax3 = plt.subplot(gs[2])
+violin_parts = ax3.violinplot(tau_est, showmeans=True, showmedians=False, vert=False)
+ax3.set_title('Tau_est')
+change_violin_colors(violin_parts, color_list=['blue']*3)
+# change_violin_colors(violin_parts, [l.get_color() for l in ax3.get_children()[:3]])
+
+# Set tight layout
+plt.tight_layout()
 
 
+
+
+plt.figure(figsize=(15,5))
 plt.plot(H_est.T)
 plt.savefig("shiftcomponents")
 plt.clf()
+ymax = 0
+for i in range(len(H_est)):
+    ymax = max(ymax,H_est[i]/np.std(H_est[i]).max())
+yoffset = ymax
 
 for i in range(len(H_est)):
     means, sigmas, n = single_fit(H_est[i])
@@ -38,18 +68,38 @@ for i in range(len(H_est)):
     diff_matrix = calc_difference_matrix(np.ones((len(sigmas))))
     hypothesis = peak_hypothesis(diff_matrix, cutoff=10/100)
     hardmodel = Hard_Model(H_est[i], hypothesis, means, sigmas, n, lr=10, alpha = 1e-3, factor=1, patience=1, min_imp=0.01)
+
     W, C, running_loss, path, lambdas = hardmodel.fit(verbose=True, return_loss=True)
+
+    means, sigma, j_coup, mult, n = hardmodel.return_values()
+    print("means:")
+    print(means)
+    print("sigmas:")
+    print(sigma)
+    print("J coupling:")
+    print(j_coup)
+    print("multiplicity")
+    print(mult)
+    print("N")
+    print(n)
+
     print("W:")
     print(W)
     ## TODO change this to be the true underlying components from H_ART not the shift found components, they need to be scaled accordingly.
-    plt.plot(H_est[i]/np.std(H_est[i]), linewidth=5, color="k")
-
+    # plt.figure(figsize=(15,5))
+    # plt.plot(H_est[i]/np.std(H_est[i]), linewidth=5, color="k")
+    ax1.plot(x,H_est[i]/np.std(H_est[i])+yoffset*i, linewidth=4, color="k")
     for j, vec in enumerate(C):
-        plt.plot(vec*W[:,j])
-    plt.title("Component "+str(i)+" hardmodelled")
-    plt.savefig("fig"+str(i))
+        ax1.plot(x,vec*W[:,j]+yoffset*i)
+
+    plt.savefig("shiftcomponents_combined")
     plt.clf()
-    plt.title("Component "+str(i)+" regulization path")
-    plt.plot(lambdas[0], path[0].T)
-    plt.savefig("fig"+str(i)+"_path")
-    plt.clf()
+        # plt.plot(vec*W[:,j])
+    # plt.title("Component "+str(i)+" hardmodelled")
+    # plt.savefig("fig"+str(i))
+    # plt.clf()
+    # plt.figure(figsize=(15,5))
+    # plt.title("Component "+str(i)+" regulization path")
+    # plt.plot(lambdas[0], path[0].T)
+    # plt.savefig("fig"+str(i)+"_path")
+    # plt.clf()
